b3a6408070f6ef56438822bc7add095680880880
Alexander Verbruggen <alex@nabu.be>
2022-12-20 08:58:02 +0100

    initial

---
+++ component.js
@@ -0,0 +1,47 @@
+Vue.view("echarts-graph", {
+	props: {
+		page: {
+			type: Object,
+			required: true
+		},
+		parameters: {
+			type: Object,
+			required: false
+		},
+		childComponents: {
+			type: Object,
+			required: false
+		},
+		cell: {
+			type: Object,
+			required: true
+		},
+		edit: {
+			type: Boolean,
+			required: true
+		}
+	},
+	methods: {
+		configurator: function() {
+			return "echarts-graph-configure";
+		}
+	}
+});
+
+Vue.component("echarts-graph-configure", {
+	template: "#echarts-graph-configure",
+	props: {
+		page: {
+			type: Object,
+			required: true
+		},
+		cell: {
+			type: Object,
+			required: true
+		},
+		edit: {
+			type: Boolean,
+			required: true
+		}
+	}
+});
---
+++ component.tpl
@@ -0,0 +1,11 @@
+<template id="echarts-graph">
+	<div>
+		graph here!
+	</div>
+</template>
+
+<template id="echarts-graph-configure">
+	<div>
+		<data-pipelines-configure :target="cell.state" :page="page" />
+	</div>
+</template>
